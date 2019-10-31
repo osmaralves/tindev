974b60a17cc8584e59d80e7306f1b10bf8e87c6a
Osmar Alves <osmar.wd@gmail.com>
2019-10-31 00:35:09 -0300

    adiciona o axios e o cadastro de devs

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const mongodbCredentials = require('./credentials/mongodb.json');
 
 const server = express();
 
-mongoose.connect('mongodb+srv://tindev:KqLv2E2WVOZmQPQQ@cluster0-fg9nl.mongodb.net/tindev?retryWrites=true&w=majority', {
+mongoose.connect(`mongodb+srv://${mongodbCredentials.user}:${mongodbCredentials.password}@cluster0-fg9nl.mongodb.net/tindev?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
